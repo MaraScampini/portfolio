b551022018e0cb7f4452f142823912ee0aaee76c
Mara Scampini <109754827+MaraScampini@users.noreply.github.com>
2024-11-28 18:40:34 +0100

    base project and navbar

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        'gray-background': '#464646',
+        'pastel-blue': '#D1E9F6',
+        'pastel-yellow': '#F6EACB',
+        'dusty-pink': '#EECAD5',
+      },
+    },
+  },
+  plugins: [],
+}
